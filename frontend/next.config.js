62c2d300a2f9cfaa42fb84de9a2f5ce24ada2fa3
nor4e99 <nor4e99@gmail.com>
2026-03-06 20:25:33 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,13 +4,8 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com', 'images.unsplash.com'],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api'}/:path*`,
-      },
-    ];
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
   },
 };
 
